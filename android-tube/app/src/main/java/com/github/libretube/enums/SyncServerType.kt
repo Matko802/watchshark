ac399e295215ec62735353e14973dc8f867d71fa
Matko <matkohrad@gmail.com>
2026-09-22 19:35:53 +0200

    Remove all Kotlin apps (fresh Dart start)

--- android-tube/app/src/main/java/com/github/libretube/enums/SyncServerType.kt
+++
@@ -1,8 +0,0 @@
-package com.github.libretube.enums
-
-// order and names must be kept in sync with WelcomeViewModel.kt and array.xml!
-enum class SyncServerType {
-    NONE,
-    LIBRETUBE,
-    PIPED
-}
