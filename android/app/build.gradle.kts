bdf9bbb0f9b9d625d993bc43c4cd4c4623f40eb6
Matko <matkohrad@gmail.com>
2026-09-24 19:47:02 +0200

    Full-bleed square thumbs like web mobile + 1.6.42

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.watchshark.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10641
-        versionName = "1.6.41"
+        versionCode = 10642
+        versionName = "1.6.42"
         buildConfigField("String", "APP_URL", "\"https://watchshark.duckdns.org\"")
     }
 
