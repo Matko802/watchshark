a1ef60254ad68d5b7dbe8fa73e2f49d0a30e2994
Matko <matkohrad@gmail.com>
2026-09-24 17:16:30 +0200

    Fix missed side-effect imports in vendor bundle

--- public/vendor/lit._esm.js
+++ public/vendor/lit._esm.js
@@ -4,5 +4,5 @@
  *
  * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
  */
-import"/npm/@lit/reactive-element@2.1.2/+esm";import"/npm/lit-html@3.3.2/+esm";export*from"./lit-element.lit-element.js";export*from"./lit-html.is-server.js";
+import"./lit-reactive-element._esm.js";import"./lit-html._esm.js";export*from"./lit-element.lit-element.js";export*from"./lit-html.is-server.js";
 //# sourceMappingURL=/sm/2974db7c7e6551484c25fe4377ca403411404581caf6c5d6776a7d82d5e88d7b.map
