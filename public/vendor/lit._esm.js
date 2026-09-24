102bd934aeab46d430f00e9d483dc5d206b9d7d1
Matko <matkohrad@gmail.com>
2026-09-24 16:47:10 +0200

    Vendor Material+fonts locally, zero CDN deps

---
+++ public/vendor/lit._esm.js
@@ -0,0 +1,8 @@
+/**
+ * Bundled by jsDelivr using Rollup v4.62.2 and esbuild v0.28.1.
+ * Original file: /npm/lit@3.3.3/index.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+import"/npm/@lit/reactive-element@2.1.2/+esm";import"/npm/lit-html@3.3.2/+esm";export*from"./lit-element.lit-element.js";export*from"./lit-html.is-server.js";
+//# sourceMappingURL=/sm/2974db7c7e6551484c25fe4377ca403411404581caf6c5d6776a7d82d5e88d7b.map
---
+++ public/vendor/lit.decorators.js
@@ -0,0 +1,8 @@
+/**
+ * Bundled by jsDelivr using Rollup v4.62.2 and esbuild v0.28.1.
+ * Original file: /npm/lit@3.3.3/decorators.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+export*from"./lit-reactive-element.decorators.custom-element.js";export*from"./lit-reactive-element.decorators.property.js";export*from"./lit-reactive-element.decorators.state.js";export*from"./lit-reactive-element.decorators.event-options.js";export*from"./lit-reactive-element.decorators.query.js";export*from"./lit-reactive-element.decorators.query-all.js";export*from"./lit-reactive-element.decorators.query-async.js";export*from"./lit-reactive-element.decorators.query-assigned-elements.js";export*from"./lit-reactive-element.decorators.query-assigned-nodes.js";
+//# sourceMappingURL=/sm/1795eab5c57f6d6366d9a3a6e814f49e2913a6e4772390c5c641e965ca473fe3.map
